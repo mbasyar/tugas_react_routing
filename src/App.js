0fb6767300ed69cffb63efcd8864d4b2f7033472
mbasyar <muhammadkhoirulb@gmail.com>
2023-03-27 22:51:10 +0700

    eror mulu pusing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import './App.css';
 import { Link, Outlet } from 'react-router-dom';
+import React from 'react';
+// import { Cemilan } from './pages/cemilan';
+// import { Makanan } from './pages/makanan';
+// import { Minuman } from './pages/minuman';
 
 
 function App() {
   return (
     <div>
-      <h1>BELAJAR REACT ROUTER</h1>
-      <nav>
-        <Link to={"Makanan"}> MAKANAN </Link>
-        <Link to={"Minuman"}> MINUMAN</Link>
-        <Link to={"Cemilan"}> CEMILAN</Link>
-      </nav>
-        
+      <div className='container'>
+        <h1>BELAJAR REACT ROUTER</h1>
+        <nav className='navbar'>
+          <Link to={"Makanan"} className="me"> MAKANAN </Link>
+          <Link to={"Minuman"} className="me"> MINUMAN</Link>
+          <Link to={"Cemilan"} className="me"> CEMILAN</Link>
+        </nav>
+          <Outlet /> 
 
-        <Outlet />
+         
+      </div>
     </div>
   
   );
--- src/komponen/food.jsx
+++ src/komponen/food.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import {food} from '../asset/kue2.jpg';
 
 export const Food = () => {
   return (
     <div>
         <h1>ini halaman food</h1>
+        <img src={food} alt="food" />
     </div>
   )
 }
